7eddb1068ed00f8c4cc26369625bbd8acf1ce5a4
HendrickFS <hendrickfel@gmail.com>
2023-10-20 18:25:24 -0300

    serializer, viewset e admin

---
+++ backend/product/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, primary_key=True)
+    price = models.FloatField(null=True)
+    stock = models.IntegerField(default=0)
+    request = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
