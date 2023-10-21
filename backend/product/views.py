2f09bab01513b20dd6731b313b4c647f80ed268f
HendrickFS <hendrickfel@gmail.com>
2023-10-20 21:02:24 -0300

    api criada

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,3 +1,80 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Product
+from .api.serializers import ProductSerializer
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework import status
+
+from django.contrib.auth.hashers import make_password, check_password
+
+class ProductAPIView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        if(Product.objects.filter(name=request.data['name']).exists()):
+            return Response({
+                'error': True,
+                'message': 'Produto já cadastrado!'
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                'error': False,
+                'message': 'Produto cadastrado com sucesso!'
+            }, status=status.HTTP_201_CREATED)
+        
+        if serializer.errors:
+            if('name' not in request.data):
+                return Response({
+                    'error': True,
+                    'message': 'O produto precisa de um nome!'
+                }, status=status.HTTP_400_BAD_REQUEST)
+
+            return Response({
+                'error': True,
+                'message': 'Erro ao cadastrar produto!'
+            }, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request):
+        if(not Product.objects.filter(name=request.data['name']).exists()):
+            return Response({
+                'error': True,
+                'message': 'Produto não cadastrado!'
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        product = Product.objects.get(name=request.data['name'])
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                'error': False,
+                'message': 'Produto atualizado com sucesso!'
+            }, status=status.HTTP_200_OK)
+        
+        if serializer.errors:
+            return Response({
+                'error': True,
+                'message': 'Erro ao atualizar produto!'
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self, request):
+        if(not Product.objects.filter(name=request.GET).exists()):
+            return Response({
+                'error': True,
+                'message': 'Produto não cadastrado!'
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        product = Product.objects.get(name=request.GET)
+        product.delete()
+        return Response({
+            'error': False,
+            'message': 'Produto deletado com sucesso!'
+        }, status=status.HTTP_200_OK)
